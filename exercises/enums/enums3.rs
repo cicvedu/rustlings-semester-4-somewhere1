8f1d56bb20dcd2abcd54e31fedec18a00fddbc64
somewhere1 <49219756+somewhere1@users.noreply.github.com>
2024-07-10 01:36:02 +0000

    fisrt submit

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -3,12 +3,14 @@
 // Address all the TODOs to make the tests pass!
 //
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
-// hint.
 
-// I AM NOT DONE
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    ChangeColor(i32,i32,i32),
+    Quit,
 }
 
 struct Point {
@@ -43,6 +45,20 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+       match  message {
+            Message::ChangeColor(a,b,c) => {
+                self.change_color((a as u8,b as u8,c as u8));
+            },
+            Message::Move(P) =>{
+                        self.move_position(P);
+            },
+            Message::Echo(s) =>{
+                self.echo(s);
+            }
+            Message::Quit =>{
+                self.quit();
+            }
+       }
     }
 }
 
--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,7 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +15,10 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) =>  {
+            let capitalized = first.to_uppercase().collect::<String>();
+            capitalized + c.as_str()
+        },
     }
 }
 
@@ -24,7 +27,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|&c| capitalize_first(c)).collect()
 }
 
 // Step 3.
@@ -32,7 +35,8 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+   
+    words.iter().map(|&word| capitalize_first(word)).collect::<Vec<String>>().join("")
 }
 
 #[cfg(test)]
